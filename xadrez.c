a7a1db49cbcc9ba610ea3386be58887b7aa28a6c
melorenata <melo.renata.pro@gmail.com>
2025-10-15 14:51:23 -0300

    desafio mestre
    Added functions to handle movements for Torre, Bispo, Rainha, and Cavalo pieces in a chess game.

--- xadrez.c
+++ xadrez.c
@@ -1,75 +1,85 @@
 #include <stdio.h>
 
-int main() {
-    int mov_torre, mov_bispo, mov_rainha; // def de qtd de movimento das peças
-    int i=0, a=0; // contador
-    
-    // TORRE
-    printf("Digite a quantidade de movimento(s) da Torre: \n");
-    scanf("%d", &mov_torre);
-    
-    // condição while que apresenta o movimento
-    while(i < mov_torre){
-        printf("Direita\n");
-        i++;
+//função mover torre
+void moverTorre(int casas){ // mover 5 casas para direita
+    if(casas > 0){
+        printf("Movimento para a direita\n");
+        moverTorre(casas - 1);
     }
-    
-    // BISPO
-    printf("Digite a quantidade de movimento(s) do Bispo: \n");
-    scanf("%d", &mov_bispo);
-    // condição for que apresenta o movimento do bispo 
-    for(i=0;i<mov_bispo;i++){
-        printf("Cima e direita\n");
+}
+
+//função mover bispo
+void moverBispo(int casas){ // mover 5 casas diagonal direita pra cima
+    if(casas > 0){
+        printf("movimento na diagonal direita e cima\n");
+        moverBispo(casas - 1);
     }
-    
-    // RAINHA
-    printf("Digite a quantidade de movimento(s) da Rainha: \n");
-    scanf("%d", &mov_rainha);
-    // condição do while para movimentar a rainha
-    do{
+}
+
+//função mover rainha
+void moverRainha(int casas){ // mover 8 casas para esquerda
+    if(casas > 0){
+        printf("Movimento para a esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+
+//função mover cavalo
+void moverCavalo(int valor){ // mover uma vez pra cima e direita
+    if(valor == 1){
+    for(int i =0;i<2;i++){
+        printf("Movimento para cima\n");
+    }
+    printf("Movimento para direita");
+    }if(valor == 2){
+        for(int i = 0;i<2;i++){
+            printf("Cima\n");
+        }
         printf("Esquerda\n");
-        a++;
-    }while(a<mov_rainha);
+        }if(valor == 3){
+        for(int i = 0;i<2;i++){
+            printf("Baixo\n");
+        }
+        printf("Direita\n");
+        }if(valor == 4){
+        for(int i = 0;i<2;i++){
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+        }
+}
+
+int main() {
+    int num;
     
+    // movimento da Torre chamada por função
+    printf("Digite a quantidade de casas para a Torre se mover.\n");
+    scanf("%d", &num);
+    moverTorre(num);
+    
+    printf("\n\n");
+    // movimento do Bispo chamada por função
+    printf("Digite a quantidade de casas para o Bispo se mover.\n");
+    scanf("%d", &num);
+    moverBispo(num);
+    
+    printf("\n\n");
+    // movimento da Rainha chamada por função
+    printf("Digite a quantidade de casas para o Bispo se mover.\n");
+    scanf("%d", &num);
+    moverRainha(num);
     
     // CAVALO
     printf("\n\n");
+    printf("Movimento do Cavalo\n");
     //opções para escolha
     printf("Digite 1 para movimento para cima e direita \n"); 
     printf("Digite 2 para movimento para cima e esquerda \n");
     printf("Digite 3 para movimento para baixo e direita \n");
     printf("Digite 4 para movimento para baixo e esquerda \n");
-    int valor;
-    scanf("%d", &valor);
-    
-    int mov_cavalo = 1; // controle de movimento
-    //condição para movimentos
-    while(mov_cavalo--){
-        if(valor == 1){
-        for(int i = 0;i<2;i++){
-            printf("Cima\n");
-        }
-        printf("Direita\n");
-        }
-        if(valor == 2){
-        for(int i = 0;i<2;i++){
-            printf("Cima\n");
-        }
-        printf("Esquerda\n");
-        }
-        if(valor == 3){
-        for(int i = 0;i<2;i++){
-            printf("Baixo\n");
-        }
-        printf("Direita\n");
-        if(valor == 4){
-        for(int i = 0;i<2;i++){
-            printf("Baixo\n");
-        }
-        printf("Esquerda\n");
-        }
-        }
-    }
+    int escolha;
+    scanf("%d", &escolha);
+    moverCavalo(escolha);
     
     return 0;
 }
